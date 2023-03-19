6e1a4a16c65f10e3581fdbcddabee630afc730b1
Oli <oli.varnavska@gmail.com>
2023-03-19 14:38:30 +0800

    Add Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿//  void SayLang(string lang)
+//  {
+
+//  Console.WriteLine($"Lang {lang}!!!");
+
+//   }
+// SayLang("Python");
+
+// SayLang("C#");
+
+// SayLang("PHP");
+
+// string x = "C++";
+
+// SayLang(x);
+
+// SayLang("Java");
+
+int SayMax(int num1, int num2)
+{
+    if (num1 > num2)
+    {
+        num1 = num1+10;
+        return num1 + 10;
+    }
+    else
+    {
+        num2 = num2 * 2;
+        return num2 * 2;
+    }
+}
+Console.WriteLine($"{SayMax(5, 3)}");
+int x1 = SayMax(2, 7);
+Console.WriteLine($"{x1}");
+
+
