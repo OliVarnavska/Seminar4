ba3e7062cb3fd051c20b4fcb1a1629700c69e923
Oli <oli.varnavska@gmail.com>
2023-03-19 16:01:12 +0800

    Fix Task 28

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+ 
+//  Задача 28.
+//  Напишите программу, которая принимает на вход число N 
+//  и выдает произведение чисел от 1 до N.
+  
+//   4 -> 24
+//   5 -> 120
